16dea2af11706728990f045ec00f0981ae6132cb
Damir Gafic <44035881+damirgafic@users.noreply.github.com>
2020-09-30 11:39:57 -0700

    Update jeopardy.py
    Cleaned up code

--- jeopardy.py
+++ jeopardy.py
@@ -17,20 +17,6 @@ class Category:  # setting up all elements of category here
         self.points = points
 
 
-'''response = requests.get('http://jservice.io//api/category?id=11531').json()
-
-print(response)
-for key, value in response[0].items():
-    print(key, value)'''
-
-# print(response[0]['question'])
-'''user_answer = input('What is: ')
-if user_answer == response[0]['answer']:
-    print('success! ' + str(response[0]['value']) + ' added to score')
-else:
-    print('incorrect ' + str(response[0]['value']) + ' deducted from score')
-'''
-
 
 def newGame():
     player1 = Player()
@@ -48,29 +34,6 @@ def board(cat1):
     print('|' + '    ' + '    ' + '    ''|')
     print('|' + '    ' + '    ' + '    ''|')
     print('|' + '    ' + '    ' + '    ''|')
-
-
-
-'''def setupBoard():
-    id = []
-    questions = []
-    answers = []
-    value = []
-    for i in range(5):
-        response = requests.get('http://jservice.io/api/random?count=1').json()
-        # time.sleep(200)
-        id.append(response[0]['category']['id'])
-        print(id)  # test code
-    response = requests.get('http://jservice.io//api/category?id=' + str(id[i])).json()
-    for i in range(5):
-        questions.append(response['clues'][i]['question'])
-        answers.append(response['clues'][i]['answer'])
-        value.append(response['clues'][i]['value'])
-
-    name = (response['title'])
-    cat1 = Category(name, questions, answers, value)
-
-    print('ok') '''
 
 
 def helper(res, x):
@@ -104,24 +67,10 @@ def createCatInst():  # function to create class object of category
 
     return cat1
 
-   # print(cat1.name)
-    #print(cat1.questions)
-    #print(cat1.answers)
-
-
-
-
-#name = input('Welcome to Jeapardy! What is your name? ')
-#p1 = Player(name)
-
-#print('Welcome ' + p1.name + '! Here are your categories today!')
 
 cat1 = createCatInst()
 cat2 = createCatInst()
 cat3 = createCatInst()
 cat4 = createCatInst()
 cat5 = createCatInst()
-print(cat1.questions[2])
-#print(cat1)
-#board(cat1)
-# cat3 = createCatInst()
+
